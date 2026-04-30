93dcf05f49a905713808d1a63872cb8a264a1ea8
sneha131107 <snehaullam@gmail.com>
2026-04-30 11:57:54 +0530

    Add files via upload

---
+++ spy.py
@@ -0,0 +1,13 @@
+n=int(input("Enter a number:"))
+sum_digits=0
+product_digits=1
+temp=n
+while temp>0:
+    digit=temp%10
+    sum_digits+=digit
+    product_digits*=digit
+    temp=temp//10
+if sum_digits==product_digits:
+    print("Spy Number")
+else:
+    print("Not a Spy Number") 
