da654deb56d64650d13ccf76a5951dbeccd45317
sneha131107 <snehaullam@gmail.com>
2026-04-30 11:47:37 +0530

    Add files via upload

---
+++ without arguments wothout return value.py
@@ -0,0 +1,8 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+==================================================== RESTART: C:/Users/DELL/Desktop/python/fun ex.py ====================================================
+50
+>>> 
+===================================== RESTART: C:/Users/DELL/Desktop/python/with arguments without return values.py =====================================
+80
